147ca9dc8429128f0c659a9d37ad6f8d246de3b7
Rami <aboomar.2@osu.edu>
2022-01-08 19:19:31 -0500

    "updated ui'

--- app.js
+++ app.js
@@ -1,41 +1,77 @@
+const scissors = document.querySelector('#scissors')
+const paper = document.querySelector('#paper')
+const rock = document.querySelector('#rock')
+const update = document.querySelector('.update')
+const playerScore1 = document.querySelector('.playerScore1')
+const computerScore1 = document.querySelector('.computerScore1')
+let playerSelection = 'nothing'
+
+rock.addEventListener('click', ()=> {
+     playerSelection = 'rock'
+    game()
+})
+paper.addEventListener('click', ()=> {
+    playerSelection = 'paper'
+    game()
+})
+scissors.addEventListener('click', ()=> {
+    playerSelection = 'scissors'
+   game()
+})
+
 const pick = {1:"rock" , 2:"paper", 3:"scissors"}
 
 function computerPlay (){
     let randNum = Math.floor(Math.random()*3)+1;
     return pick[randNum]
 }
+let computerScore = 0;
+let playerScore = 0; 
+
 function game (){
-    let computerScore = 0;
-    let playerScore = 0; 
-    for (let i =0; i<5; i++){
-function playRound(playerSelection, computerSelection){
+    
+    function playRound(playerSelection, computerSelection){ 
     if (playerSelection == "scissors" && computerSelection =="paper"){
         playerScore ++
-        return `You Won! ${playerSelection} beats ${computerSelection}`
+        update.innerText = `You Won! this round ${playerSelection} beats ${computerSelection}`
+        return playerScore1.innerText = playerScore
     }
     if (playerSelection == "paper" && computerSelection =="rock"){
         playerScore ++
-        return `You Won! ${playerSelection} beats ${computerSelection}`
+        update.innerText = `You Won! this round ${playerSelection} beats ${computerSelection}`
+        return playerScore1.innerText = playerScore
     }
     
     if (playerSelection == "rock" && computerSelection =="scissors"){
         playerScore ++
-        return `You Won! ${playerSelection} beats ${computerSelection}`
+        update.innerText = `You Won! this round ${playerSelection} beats ${computerSelection}`
+        return playerScore1.innerText = playerScore
     }
     if (playerSelection == computerSelection)
-    { return `its a tie ${playerSelection} ${computerSelection}`}
+    { update.innerText = `its a tie ${playerSelection} ${computerSelection}`}
     else {
         computerScore ++
-        return `You Lose ${computerSelection} beats ${playerSelection}`
+        update.innerText = `You Lose this round ${computerSelection} beats ${playerSelection}`
+        return computerScore1.innerText = computerScore
     }
-}
-
-const playerSelection = prompt('Please, pick one Rock, Paper, Scissors').toLowerCase()
-const computerSelection = computerPlay()
-
-console.log(playRound(playerSelection, computerSelection)) } 
-if (computerScore>playerScore){return `You lose ${computerScore} to ${playerScore}`}
-else {
-    return `You won ${playerScore} to ${computerScore}`
-}
-}
+    }
+    const computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection)
+    if (computerScore ==5){
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        return update.innerText='You Lost the computer got to 5 first'
+        }
+    if (playerScore ==5){
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        return update.innerText='You won! you got to 5 first'
+        }
+ } 
+// if (computerScore>playerScore){
+//     return `You lose ${computerScore} to ${playerScore}`}
+// else {
+//     return `You won ${playerScore} to ${computerScore}`
+// }
